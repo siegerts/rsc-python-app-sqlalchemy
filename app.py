6de8b83428f79e3ba6100e5209ce69b89045db68
Stephen <stephen.siegert@gmail.com>
2020-03-17 09:59:25 -0400

    fix close quote

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"{os.environ("DATABASE_URI")}
+app.config["SQLALCHEMY_DATABASE_URI"] = f"{os.environ("DATABASE_URI")}"
 
 db = SQLAlchemy(app)
 
