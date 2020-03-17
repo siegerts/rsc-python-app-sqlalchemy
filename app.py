c27f1cc567cac168a1bc62100d76fbfb423e8362
Stephen <stephen.siegert@gmail.com>
2020-03-17 09:34:44 -0400

    add flask-sqlalchemy

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ("DATABASE_URI")
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+
+
+db.session.add(User(name="Flask", email="example@example.com"))
+db.session.commit()
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    users = User.query.all()
+    return render_template('index.html', users=users)
